1db17f4da6593bbda43c7fe20d3355b4417f28b4
hiratasa <36002166+hiratasa@users.noreply.github.com>
2020-09-05 18:01:24 +0900

    Add dummy index and static files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::Files;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/css", "./public/css"))
+            .route("/", web::get().to(index))
+    })
+    .bind("localhost:8080")?
+    .run()
+    .await
 }
