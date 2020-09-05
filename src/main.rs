d86b91c6ee6bf00c7c32f8ec72343aecbc7171d8
hiratasa <36002166+hiratasa@users.noreply.github.com>
2020-09-05 18:12:49 +0900

    Add TODO comments

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@ use actix_files::Files;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn index() -> impl Responder {
+    // TODO:
+    //  - Display TOP10 and the worst.
+    //  - Display result for each party, by descending order.
+    //  - Display ratio for each gender.
     HttpResponse::Ok().body("Hello!")
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    // TODO:
+    //  - prepare DB
+    //  - prepare session
     HttpServer::new(|| {
         App::new()
             .service(Files::new("/css", "./public/css"))
