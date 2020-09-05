fcc10f0602a61b0414fdbed03829415442279bda
hiratasa <36002166+hiratasa@users.noreply.github.com>
2020-09-05 21:50:25 +0900

    Render templates with dummy variables

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,109 @@ mod helpers;
 
 use actix_files::Files;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use handlebars::Handlebars;
+use serde_json::json;
 
-async fn index() -> impl Responder {
+async fn index(hb: web::Data<Handlebars<'_>>) -> impl Responder {
     // TODO:
     //  - Display TOP10 and the worst.
     //  - Display result for each party, by descending order.
     //  - Display ratio for each gender.
-    HttpResponse::Ok().body("Hello!")
+    let data = json!({
+        "candidates": [
+            {
+                "id": 0,
+                "name": "dummy",
+                "vote_count": 0,
+                "political_party": "dummy_party",
+            }
+        ],
+        "parties": [
+            {
+                "political_party": "dummy_party",
+                "vote_count": 0,
+            }
+        ],
+        "sex_ratio": {
+            "men": 0,
+            "women": 0,
+        },
+    });
+
+    HttpResponse::Ok().body(hb.render("index", &data).unwrap())
 }
 
-async fn show_candidate(id: web::Path<(String,)>) -> impl Responder {
+async fn show_candidate(
+    hb: web::Data<Handlebars<'_>>,
+    _id: web::Path<(String,)>,
+) -> impl Responder {
     // TODO:
     //  - Display vote count.
     //  - Display voice of supportes.
-    id.0.clone()
+    let data = json!({
+        "candidate": {
+            "name": "dummy",
+            "political_party": "dummy_party",
+            "sex": "",
+        },
+        "votes": 0,
+        "keywords": ["dummy_keyword"],
+    });
+
+    HttpResponse::Ok().body(hb.render("candidate", &data).unwrap())
 }
 
-async fn show_political_party(name: web::Path<(String,)>) -> impl Responder {
+async fn show_political_party(
+    hb: web::Data<Handlebars<'_>>,
+    _name: web::Path<(String,)>,
+) -> impl Responder {
     // TODO:
     //  - Display vote count for the party.
     //  - Display candidates of the party.
     //  - Display voice of supporters of the candidates.
-    name.0.clone()
+    let data = json!({
+        "political_party": "",
+        "votes": 0,
+        "candidates": [
+            {
+                "name": "dummy",
+            }
+        ],
+        "keywords": ["dummy_keyword"],
+    });
+
+    HttpResponse::Ok().body(hb.render("political_party", &data).unwrap())
 }
 
-async fn show_vote() -> impl Responder {
+async fn show_vote(hb: web::Data<Handlebars<'_>>) -> impl Responder {
     // TODO: Display all candidates
-    "not implemented"
+    let data = json!({
+        "candidates": [
+            {
+                "name": "dummy",
+            }
+        ],
+        "message": "",
+    });
+
+    HttpResponse::Ok().body(hb.render("vote", &data).unwrap())
 }
 
-async fn do_vote() -> impl Responder {
+async fn do_vote(hb: web::Data<Handlebars<'_>>) -> impl Responder {
     // TODO:
     //  - Fetch form values.
     //  - Validate user info, user vote upper bound, candidate name and keyword.
     //  - Execute vote (insert to DB)
-    "not implemented"
+    let data = json!({
+        "candidates": [
+            {
+                "name": "dummy",
+            }
+        ],
+        "message": "",
+    });
+
+    HttpResponse::Ok().body(hb.render("vote", &data).unwrap())
 }
 
 async fn initialize() -> impl Responder {
@@ -50,8 +118,16 @@ async fn main() -> std::io::Result<()> {
     // TODO:
     //  - prepare DB
     //  - prepare session
-    HttpServer::new(|| {
+
+    let mut hb = Handlebars::new();
+    hb.register_templates_directory(".hbs", "./templates/")
+        .unwrap();
+    hb.register_helper("plus1", Box::new(helpers::plus1));
+    let hb = web::Data::new(hb);
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(hb.clone())
             .service(Files::new("/css", "./public/css"))
             .route("/", web::get().to(index))
             .route("/candidates/{id}", web::get().to(show_candidate))
